3fcf5b4cfb0f116debf895c32c7c89055a9cc9ae
wwwxtus <vovadoroshenko228@list.ru>
2023-04-07 23:14:50 +0300

    Task3

---
+++ libs/data_structures/vector/vector.c
@@ -0,0 +1,5 @@
+//
+// Created by vovad on 07.04.2023.
+//
+
+#include "vector.h"
---
+++ libs/data_structures/vector/vector.h
@@ -0,0 +1,8 @@
+//
+// Created by vovad on 07.04.2023.
+//
+
+#ifndef VECTORC_VECTOR_H
+#define VECTORC_VECTOR_H
+
+#endif //VECTORC_VECTOR_H
