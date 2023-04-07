7a41c230c5d3d4f3f2506dc3d5f68aeba6c9c06a
wwwxtus <vovadoroshenko228@list.ru>
2023-04-07 23:26:38 +0300

    Task3

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -2,3 +2,15 @@
 #include <stdio.h>
 #include "vector.h"
 
+Vector createVector(int n) {
+    if (n >= 1000000000) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+
+    return (Vector) {
+            NULL,
+            0,
+            n
+    };
+}
